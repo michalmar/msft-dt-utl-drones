3dfb1c8431d99f1c6f6efd0f2f0c46668ce19616
Ubuntu <azureuser@drones-dev66c8084969a.4ipghuzjfmvevlkdqag4vk5srh.ax.internal.cloudapp.net>
2019-10-08 09:48:50 +0000

    updated model download

--- download_model_aml.py
+++ download_model_aml.py
@@ -18,13 +18,13 @@ from azureml.core.authentication import InteractiveLoginAuthentication
 print("SDK version:", azureml.core.VERSION)
 
 # due to diferent tenant -> typically customer tenant
-interactive_auth = InteractiveLoginAuthentication(tenant_id="0f277086-d4e0-4971-bc1a-bbc5df0eb246")
-
+# interactive_auth = InteractiveLoginAuthentication(tenant_id="0f277086-d4e0-4971-bc1a-bbc5df0eb246")
+interactive_auth = InteractiveLoginAuthentication(tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47") # MSFT tenant
 ws = Workspace.from_config()
 print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep='\n')
 
 
-exp = ws.experiments['gc-yolo3-20190808']
+exp = ws.experiments['drones-yolo3']
 
 
 def getAMLRun(exp, runId = None):
@@ -53,7 +53,9 @@ xrun = getAMLLastRun(exp)
 
 # download model -> download rather to blob
 if not(os.path.exists(os.path.join("outputs","trained_weights_final.h5"))):
+    print(f"downloading model from experiment {exp.name}...")
     xrun.download_file(name="logs/000/trained_weights_final.h5", output_file_path="outputs")
+    print("done.")
 else:
     print(f'target file already exists {os.path.exists(os.path.join("outputs","trained_weights_final.h5"))}')
 
